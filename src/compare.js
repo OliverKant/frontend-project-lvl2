402aa80dd93e3de16542c39efb77f9138d1fe538
Dmitriy <3815300@gmail.com>
2022-04-18 19:11:14 +0300

    edits lint

--- src/compare.js
+++ src/compare.js
@@ -6,9 +6,8 @@ const sign = {
 };
 
 const compare = (file1, file2) => {
-
-  const keys1 = _.keys(file1);
-  const keys2 = _.keys(file2);
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
 
   const equalKeys = _.union(keys1, keys2);
   const sortedKeys = _.sortBy(equalKeys);
@@ -16,13 +15,14 @@ const compare = (file1, file2) => {
   const result = sortedKeys.map((key) => {
     if (file1[key] === file2[key]) {
       return `    ${key}: ${file1[key]}`;
-    } else if (_.has(file1, key) && !_.has(file2, key)) {
-      return `  ${sign.minus} ${key}: ${file1[key]}`;
-    } else if (!_.has(file1, key) && _.has(file2, key)) {
-      return `  ${sign.plus} ${key}: ${file2[key]}`;
-    } else if (file1[key] !== file2[key]) {
-      return `  ${sign.minus} ${key}: ${file1[key]}\n  ${sign.plus} ${key}: ${file2[key]}`;
     }
+    if (_.has(file1, key) && !_.has(file2, key)) {
+      return `  ${sign.minus} ${key}: ${file1[key]}`;
+    }
+    if (!_.has(file1, key) && _.has(file2, key)) {
+      return `  ${sign.plus} ${key}: ${file2[key]}`;
+    }
+    return `  ${sign.minus} ${key}: ${file1[key]}\n  ${sign.plus} ${key}: ${file2[key]}`;
   }).join('\n');
   return `{\n${result}\n}`;
 };
