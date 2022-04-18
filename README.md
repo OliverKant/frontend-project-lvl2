19d2022aa172e9f2d885e54a444d6aacf28f9a75
Dmitriy <3815300@gmail.com>
2022-04-18 17:53:24 +0300

    add codeclimat, github actions

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/OliverKant/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/OliverKant/frontend-project-lvl2/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/684deb691facf892e364/maintainability)](https://codeclimate.com/github/OliverKant/frontend-project-lvl2/maintainability)
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import path from "path";
-import {readFileSync} from 'fs';
-import compare from "./compare.js";
+import path from 'path';
+import { readFileSync } from 'fs';
+import compare from './compare.js';
 
 const parse = (files) => {
   const fullPath = path.resolve(process.cwd(), files);
-  const data = readFileSync(fullPath, {encoding: "utf-8"});
+  const data = readFileSync(fullPath, { encoding: 'utf-8' });
   return JSON.parse(data);
-}
+};
 
 const genDiff = (fileName1, fileName2) => {
-
   const parse1 = parse(fileName1);
   const parse2 = parse(fileName2);
 
   return compare(parse1, parse2);
-}
+};
 
 export default genDiff;
